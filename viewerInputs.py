b7ea515f9a7c360c24d08adf24cfca3508437c30
lowerfield@gmail.com <manuelperez@manuels-mbp.config>
2015-08-20 00:01:35 +0100

    new viewer fix

--- viewerInputs.py
+++ viewerInputs.py
@@ -1,14 +1,36 @@
 import nuke
 
-
 __all__ = ['viewerInputs']
+
+viewerList = [] 
+
+def viewerInputsToogle():
+
+    for node in nuke.allNodes('Viewer'):
+
+        if node['hide_input'].value() == False:
+            node['hide_input'].setValue(True)
+
+        elif node['hide_input'].value() == True:
+            node['hide_input'].setValue(False)
+
+    for node in nuke.allNodes('Viewer'):
+        viewerList.append(node)
 
 def viewerInputs():
 
-	for node in nuke.allNodes('Viewer'):
+    currentViewers = nuke.allNodes('Viewer')
 
-	    if node['hide_input'].value() == False:
-	        node['hide_input'].setValue(True)
+    if viewerList == []:
+        viewerInputsToogle()
+           
+    elif viewerList != [] and viewerList == currentViewers:
+        viewerInputsToogle()
+            
+    elif viewerList != [] and viewerList != currentViewers:
 
-	    elif node['hide_input'].value() == True:
-	        node['hide_input'].setValue(False)
+        for node in list(set(nuke.allNodes('Viewer')).difference(viewerList)):
+            node['hide_input'].setValue(True)
+
+        viewerInputsToogle()
+    
